414193f782fe19080c0a022c1e48db6e4992ad49
Ruan thiago <ruanthiago.in@gmail.com>
2022-08-20 22:38:52 -0300

    Project hr-worker created

---
+++ hr-worker/src/main/java/com/DevM3c/hrworker/HrWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.DevM3c.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrWorkerApplication.class, args);
+    }
+
+}
---
+++ hr-worker/src/test/java/com/DevM3c/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevM3c.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
