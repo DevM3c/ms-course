02ce70ebb7fb6ae90a91de904285c3bf0f385ddf
Ruan thiago <ruanthiago.in@gmail.com>
2022-08-21 16:41:19 -0300

    Rest Template

---
+++ hr-payroll/src/main/java/com/devm3c/hrpayroll/AppConfig.java
@@ -0,0 +1,15 @@
+package com.devm3c.hrpayroll;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
